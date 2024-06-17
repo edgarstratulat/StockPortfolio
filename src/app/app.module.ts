b0ae83a36ae5bf8aceb16c6844a1985cfcc95d42
Edgar Sousa <stratulatedgar@gmail.com>
2024-06-17 09:40:32 +0100

    Inicial Angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioTableComponent } from './portfolio-table/portfolio-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/portfolio-table/portfolio-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-portfolio-table',
+  templateUrl: './portfolio-table.component.html',
+  styleUrl: './portfolio-table.component.css'
+})
+export class PortfolioTableComponent {
+
+}
